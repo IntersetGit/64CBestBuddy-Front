d56d664a5b477bdc319846f8ed2798c3cb6fcf5c
pondkarun <pondkarun@gmail.com>
2021-07-05 02:43:20 +0700

    excel

--- routes/insurance/manage.js
+++ routes/insurance/manage.js
@@ -15,11 +15,14 @@ const InsuranceManage = (props) => {
         percentage: null,
         is_one_price: true,
     })
+    const [finishConfig, setfinishConfig] = useState(false)
     const [masterdata, setMasterdata] = useState({
         Type: [],
         AgeRang: [],
         Installment: [],
     })
+
+    const [insurancePrice, setInsurancePrice] = useState([])
 
 
     useEffect(() => {
@@ -30,7 +33,12 @@ const InsuranceManage = (props) => {
     const onInit = async () => {
         try {
             const _res = await GetMasterInsuranceService()
-            setMasterdata(_res.data.items)
+            setMasterdata({
+                ...masterdata,
+                Type: _res.data.items.Type.data,
+                AgeRang: _res.data.items.AgeRang,
+                Installment: _res.data.items.Installment,
+            })
 
         } catch (error) {
             message.error('เรียกข้อมูลผิดพลาด!');
@@ -130,51 +138,152 @@ const InsuranceManage = (props) => {
             const Index = _data.findIndex(e => e.id === x)
             if (Index != -1) value.push(_data[Index])
         })
-        console.log('value :>> ', value);
+        // console.log('value :>> ', value);
         setAgeRangList(value)
     }
 
 
     /* Excel */
     const downloadTemplateExcel = () => {
+        try {
+            const ageRange_arr = ageRangeList //ช่วงอายุ 
+            const installmentList_arr = installmentList //งวด ระยะเวลา
+            const masPlan_arr = masPlan //แผ่นประกัน 
+            const gender_arr = (checkModel.is_one_price) ? ["-"] : ["ชาย", "หญิง"] //เพศ 
 
-        const ageRange_arr = ageRangeList //ช่วงอายุ 
-        const installmentList_arr = installmentList //งวด ระยะเวลา
-        const masPlan_arr = masPlan //แผ่นประกัน 
-        const gender_arr = (checkModel.is_one_price) ? ["-"] : ["ชาย", "หญิง"] //เพศ 
-
-        console.log('1. ageRange_arr :>> ', ageRange_arr);
-        console.log('2. installmentList_arr :>> ', installmentList_arr);
-        console.log('3. masPlan_arr :>> ', masPlan_arr);
-        console.log('4. gender_arr :>> ', gender_arr);
-        const arr = []
-        ageRange_arr.forEach(a => {
-            installmentList_arr.forEach(b => {
-                masPlan_arr.forEach(c => {
-                    gender_arr.forEach(d => {
-                        arr.push({
-                            "ช่วงอายุ": a.age_range,
-                            "งวดระยะเวลา": b.name,
-                            "แผน": c.name,
-                            "เพศ": d,
-                            "ราคา": null,
-                            "ราคาส่วนลด": checkModel.percentage ? "" : "-",
-                        })
+            // console.log('1. ageRange_arr :>> ', ageRange_arr);
+            // console.log('2. installmentList_arr :>> ', installmentList_arr);
+            // console.log('3. masPlan_arr :>> ', masPlan_arr);
+            // console.log('4. gender_arr :>> ', gender_arr);
+            const arr = []
+            ageRange_arr.forEach(a => {
+                installmentList_arr.forEach(b => {
+                    masPlan_arr.forEach(c => {
+                        gender_arr.forEach(d => {
+                            arr.push({
+                                "ช่วงอายุ": a.age_range,
+                                "งวดระยะเวลา": b.name,
+                                "แผน": c.name,
+                                "เพศ": d,
+                                "ราคา": null,
+                                "ราคาส่วนลด": checkModel.percentage ? "" : "-",
+                            })
+                        });
                     });
                 });
-            });
-        })
-        console.log('arr :>> ', arr);
+            })
+            // console.log('arr :>> ', arr);
 
-        /* gen encel */
-        const workbook = XLSX.utils.book_new();
-        const worksheet = XLSX.utils.json_to_sheet(arr);
-        workbook.SheetNames.push("data");
-        workbook.Sheets["data"] = worksheet;
-        XLSX.writeFile(workbook, "Template_เบี้ยประกันภัย.xlsx");
+            /* gen encel */
+            const workbook = XLSX.utils.book_new();
+            const worksheet = XLSX.utils.json_to_sheet(arr);
+            workbook.SheetNames.push("data");
+            workbook.Sheets["data"] = worksheet;
+            XLSX.writeFile(workbook, "Template_เบี้ยประกันภัย.xlsx");
+            setfinishConfig(true)
+        } catch (error) {
+            setfinishConfig(false)
+            message.error('มีบางอย่างผิดพลาด!');
+        }
+
     }
 
+    /* importExcel */
+    const [nameFiles, setNameFiles] = useState(null);
 
+    const changeUpload = (event) => {
+        let files = event.target.files[0];
+        if (files) {
+            if (files.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+                // appDialog.showAlertCallBack("แนบไฟล์ผิดประเภท กรุณาเลือกไฟล์ตามคำแนะนำ", $scope.clear, $scope.clear)
+                alert("แนบไฟล์ผิดประเภท กรุณาเลือกไฟล์ตามคำแนะนำ")
+                clear()
+            } else {
+                setNameFiles(files.name)
+                importExcel(files)
+            }
+        } else {
+            clear()
+        }
+    }
+
+    const importExcel = (files) => {
+        if (files) {
+            const fileReader = new FileReader();
+            fileReader.onload = (event) => {
+                const data = event.target.result;
+                const workbook = XLSX.read(data, {
+                    type: "binary"
+                });
+                // console.log('workbook :>> ', workbook);
+                callBackImportExcel(workbook)
+                clear()
+            };
+            fileReader.readAsBinaryString(files);
+        }
+    }
+
+    const callBackImportExcel = async (workbook) => {
+        const sheet_name_list = workbook.SheetNames;
+        const JsonList = [];
+        sheet_name_list.forEach(async (ws) => {
+
+            workbook.Sheets[ws]['A1'].w = "age_rang";
+            workbook.Sheets[ws]['B1'].w = "installment";
+            workbook.Sheets[ws]['C1'].w = "plan";
+            workbook.Sheets[ws]['D1'].w = "gender";
+            workbook.Sheets[ws]['E1'].w = "price_normal";
+            workbook.Sheets[ws]['F1'].w = "price_sale";
+
+            const tempJson = XLSX.utils.sheet_to_json(workbook.Sheets[ws]);
+            if (tempJson.length > 0) JsonList.push(tempJson)
+
+        });
+
+
+        // console.log('1. ageRangeList :>> ', ageRangeList);
+        // console.log('2. installmentList :>> ', installmentList);
+        // console.log('3. masPlan :>> ', masPlan);
+
+        if (JsonList.length > 0) {
+            // console.log('JsonList[0] :>> ', JsonList[0]);
+            const _data = JsonList[0].map(e => {
+
+                /* ช่วงอายุ */
+                const index_age = ageRangeList.findIndex(i => i.age_range === e.age_rang);
+                /* งวด ระยะเวลา */
+                const index_installment = installmentList.findIndex(i => i.name === e.installment);
+                /* แผ่นประกัน */
+                const index_masPlan = masPlan.findIndex(i => i.name === e.plan);
+
+                if (index_age != -1 && index_installment != -1 && index_masPlan != -1) {
+                    return {
+                        id: null,
+                        insurance_id: null,
+                        mas_age_range_id: ageRangeList[index_age].id, //ช่วงอายุ 
+                        mas_age_range_name: e.age_rang, 
+                        mas_installment_id: installmentList[index_installment].id, //งวด ระยะเวลา
+                        mas_installment_name: e.installment, 
+                        mas_plan_id: masPlan[index_masPlan].id, //แผ่นประกัน 
+                        mas_plan_name: e.plan, 
+                        gender: e.gender === "-" ? "0" : e.gender === "ชาย" ? "1" : e.gender === "หญิง" ? "2" : null, //เพศ
+                        gender_name: e.gender,
+                        price_normal: e.price_normal, //ราคาปกติ
+                        price_sale: e.price_sale === "-" ? null : e.price_sale, //ราคาลด
+                    }
+                }
+            })
+            console.log('_data :>> ', _data);
+            setInsurancePrice(_data)
+        } else {
+            message.error('มีบางอย่างผิดพลาด!');
+        }
+    }
+
+    const clear = () => {
+        setNameFiles("")
+        document.getElementById('fileUpload').value = null
+    }
 
     return (
         <>
@@ -231,7 +340,7 @@ const InsuranceManage = (props) => {
                                 label="ส่วนลด (%)"
                                 name="percentage"
                             >
-                                <InputNumber style={{ width: 200 }} value={checkModel.percentage} onChange={e => setCheckModel({ ...checkModel, percentage: e })} />
+                                <InputNumber style={{ width: 200 }} value={checkModel.percentage} onChange={e => setCheckModel({ ...checkModel, percentage: e })} disabled={finishConfig} />
                             </Form.Item>
 
                             <Form.Item
@@ -239,14 +348,14 @@ const InsuranceManage = (props) => {
                                 name="is_one_price"
                             >
                                 <Switch checkedChildren="เท่ากัน" unCheckedChildren="ไม่เท่ากัน"
-                                    checked={checkModel.is_one_price} onChange={e => setCheckModel({ ...checkModel, is_one_price: e })} />
+                                    checked={checkModel.is_one_price} onChange={e => setCheckModel({ ...checkModel, is_one_price: e })} disabled={finishConfig} />
                             </Form.Item>
 
                             <Form.Item
                                 label="แผ่นประกัน"
                                 name="plan"
                             >
-                                <Select mode="tags" style={{ width: '100%' }} onChange={generateTablePlan} disabled={masProtection.length > 0} />
+                                <Select mode="tags" style={{ width: '100%' }} onChange={generateTablePlan} disabled={masProtection.length > 0 || finishConfig} />
                             </Form.Item>
 
                             {masPlan.length > 0 ? (
@@ -263,7 +372,7 @@ const InsuranceManage = (props) => {
                                 name="installment"
                             >
 
-                                <Select mode="multiple" style={{ width: '100%' }} onChange={generateAgeRange} disabled={false} >
+                                <Select mode="multiple" style={{ width: '100%' }} onChange={generateAgeRange} disabled={finishConfig} >
                                     {masterdata.AgeRang.map((e) => <Option key={e.id} value={e.id}>{e.age_range}</Option>)}
                                 </Select>
 
@@ -274,19 +383,30 @@ const InsuranceManage = (props) => {
                                 name="ageRang"
                             >
 
-                                <Select mode="multiple" style={{ width: '100%' }} onChange={generateInstallment} disabled={false} >
+                                <Select mode="multiple" style={{ width: '100%' }} onChange={generateInstallment} disabled={finishConfig} >
                                     {masterdata.Installment.map((e) => <Option key={e.id} value={e.id}>{e.name}</Option>)}
                                 </Select>
 
                             </Form.Item>
-
 
                             {installmentList.length > 0 && masPlan.length > 0 && ageRangeList.length > 0 ? (
                                 <Form.Item
                                     label="เบี้ยประกันภัย"
                                     name="premium"
                                 >
-                                    <button type="button" className="btn btn-sm btn-light" onClick={downloadTemplateExcel} >โหลด Excel</button>
+                                    {finishConfig ?
+                                        (
+                                            <>
+                                                <input class="file-upload__input" type="file" id="fileUpload" accept=".xlsx" style={{ display: "none" }} onChange={e => { changeUpload(e) }}></input>
+                                                <label className="btn btn-primary btn-sm" for="fileUpload" style={{ marginBottom: "0px" }}> อัพโหลดข้อมูล </label> &nbsp;&nbsp;
+                                                {nameFiles}
+
+                                                <button type="button" className="btn btn-light btn-sm" onClick={() => setfinishConfig(false)} >ยกเลิก</button>
+                                            </>
+                                        ) :
+                                        <button type="button" className="btn btn-sm btn-light" onClick={downloadTemplateExcel} >โหลด Template</button>
+                                    }
+
                                 </Form.Item>
                             ) : null}
 
