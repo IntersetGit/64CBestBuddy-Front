2ff982098de22045f7e7e2c0e8098f9d21d6f67c
pondkarun <pondkarun@gmail.com>
2021-07-20 01:47:10 +0700

    menu

--- components/_App/Navbar.js
+++ components/_App/Navbar.js
@@ -39,16 +39,36 @@ const NavbarThree = () => {
                 <div id="navbar" className="navbar-area navbar-three is-sticky">
 
                     <div className="main-nav">
-                        <nav id="navbar-top" className="navbar" style={{ padding: 0, margin: 0, backgroundColor: "#fff !important" }}>
+                        <div className="top-header-area" id="navbar-top">
                             <div className="container">
-                                <Link href="/">
-                                    <a onClick={toggleNavbar} className="navbar-brand">
-                                        <img src="/images/logo-bestbuddy.png" alt="logo" style={{ width: "27%", padding: 0, margin: 0 }} />
-                                    </a>
-                                </Link>
+                                <div className="row align-items-center" style={{ padding: "0 0 0 67px" }}>
+                                    <div className="col-md-4">
+                                        <div className="header-content-left">
+                                            <Link href="/">
+                                                <a onClick={toggleNavbar} className="navbar-brand">
+                                                    <img src="/images/logo-bestbuddy.png" alt="logo" style={{ width: "50%", padding: 0, margin: 0 }} />
+                                                </a>
+                                            </Link>
+                                        </div>
+                                    </div>
 
+                                    <div className="col-md-8">
+                                        <ul className="header-content-right">
+                                            <li>
+                                                <img src="/images/call-icon.png" alt="logo" style={{ width: "30%", padding: 5, margin: 0 }} />
+                                                <span><b>Call:</b> 02 276 2245-47</span>
+                                            </li>
+
+                                            <li>
+                                                <img src="/images/mail-icon.png" alt="logo" style={{ width: "21%", padding: 5, margin: 0 }} />
+                                                <span><b>Email:</b> admin@buddydbroker.co.th</span>
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
-                        </nav>
+                        </div>
 
                         <nav className="navbar navbar-expand-lg navbar-light" style={{ padding: 0, margin: 0, backgroundColor: "#31abfc !important" }}>
                             <div className="container">
@@ -66,43 +86,143 @@ const NavbarThree = () => {
                                     <span className="icon-bar bottom-bar"></span>
                                 </button>
 
+
                                 <div className={classOne} id="navbarSupportedContent">
                                     <ul className="navbar-nav m-auto">
-                                        <Link href="/" activeClassName="active">
-                                            <li className="nav-item">
-                                                <a className="nav-link">ซื้อแผนประกัน</a>
-                                            </li>
-                                        </Link>
 
-                                        <Link href={`${process.env.NEXT_PUBLIC_MENU_1}`} activeClassName="active">
-                                            <li className="nav-item">
-                                                <a li className="nav-link">บทความ</a>
-                                            </li>
-                                        </Link>
+                                        <li className="nav-item">
+                                            <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}`} >
+                                                <a onClick={toggleNavbar} className="nav-link">หน้าแรก</a>
+                                            </Link>
+                                        </li>
 
-                                        <Link href={`${process.env.NEXT_PUBLIC_MENU_2}`}>
-                                            <li className="nav-item">
-                                                <a className="nav-link">โปรโมชั่น</a>
-                                            </li>
-                                        </Link>
+                                        <li className="nav-item">
+                                            <Link href="#">
+                                                <a onClick={e => e.preventDefault()} className="nav-link">
+                                                    ผลิตภัณฑ์ <i className='bx bx-chevron-down'></i>
+                                                </a>
+                                            </Link>
 
-                                        <Link href={`${process.env.NEXT_PUBLIC_MENU_3}`}>
-                                            <li className="nav-item">
+                                            <ul className="dropdown-menu">
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ประกันโรคร้ายแรง</a>
+                                                    </Link>
+                                                </li>
 
-                                                <a className="nav-link">เกี่ยวกับเรา</a>
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ประกันมะเร็ง</a>
+                                                    </Link>
+                                                </li>
 
-                                            </li>
-                                        </Link>
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ประกันชีวิต</a>
+                                                    </Link>
+                                                </li>
 
-                                        <Link href={`${process.env.NEXT_PUBLIC_MENU_4}`}>
-                                            <li className="nav-item">
-                                                <a className="nav-link">ติดต่อเรา</a>
-                                            </li>
-                                        </Link>
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ประกันรถยนต์</a>
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                        </li>
+
+                                        <li className="nav-item">
+                                            <Link href="/about" >
+                                                <a onClick={toggleNavbar} className="nav-link">โปรโมชั่น</a>
+                                            </Link>
+                                        </li>
+
+                                        <li className="nav-item">
+                                            <Link href="#">
+                                                <a onClick={e => e.preventDefault()} className="nav-link">
+                                                    ข่าวสาร/บทความ <i className='bx bx-chevron-down'></i>
+                                                </a>
+                                            </Link>
+
+                                            <ul className="dropdown-menu">
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}/blogall/`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">บทความ</a>
+                                                    </Link>
+                                                </li>
+
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">กิจกรรมบริษัท</a>
+                                                    </Link>
+                                                </li>
+
+                                            </ul>
+                                        </li>
+
+                                        <li className="nav-item">
+                                            <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}`} >
+                                                <a onClick={toggleNavbar} className="nav-link">บริการลูกค้า</a>
+                                            </Link>
+                                        </li>
+
+                                        <li className="nav-item">
+                                            <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}/เกี่ยวกับเรา/`} >
+                                                <a onClick={toggleNavbar} className="nav-link">เกี่ยวกับเรา</a>
+                                            </Link>
+                                        </li>
+
+                                        <li className="nav-item">
+                                            <Link href="#">
+                                                <a onClick={e => e.preventDefault()} className="nav-link">
+                                                    ติดต่อเรา <i className='bx bx-chevron-down'></i>
+                                                </a>
+                                            </Link>
+
+                                            <ul className="dropdown-menu">
+                                                
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}/contact/`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ติดต่อเรา</a>
+                                                    </Link>
+                                                </li>
+
+                                                <li className="nav-item">
+                                                    <Link href={`${process.env.NEXT_PUBLIC_WORDPRESS}/ร่วมงานกับเรา/`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ร่วมงานกับเรา</a>
+                                                    </Link>
+                                                </li>
+
+                                            </ul>
+                                        </li>
+
+                                        <li className="nav-item">
+                                            <Link href="#">
+                                                <a onClick={e => e.preventDefault()} className="nav-link">
+                                                    ซื้อประกันออนไลน์ <i className='bx bx-chevron-down'></i>
+                                                </a>
+                                            </Link>
+
+                                            <ul className="dropdown-menu">
+
+                                                <li className="nav-item">
+                                                    <Link href={`/`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ซื้อประกัน Falcon</a>
+                                                    </Link>
+                                                </li>
+
+                                                <li className="nav-item">
+                                                    <Link href={`https://www.falconinsurance.co.th/index.php`} >
+                                                        <a onClick={toggleNavbar} className="nav-link">ซื้อประกัน Cigna</a>
+                                                    </Link>
+                                                </li>
+
+                                            </ul>
+                                        </li>
 
                                     </ul>
 
                                 </div>
+
                             </div>
                         </nav>
                     </div>
